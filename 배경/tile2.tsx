7e384f4c5a734e159ae7c4ecfe7e55a679be54b8
nicole <dldkdud0771@gmail.com>
2023-08-05 17:29:37 +0900

    배경

---
+++ 배경/tile2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tile2" tilewidth="30" tileheight="30" tilecount="25" columns="5">
+ <image source="../../../Downloads/타일셋리소스2.png" width="150" height="150"/>
+</tileset>
