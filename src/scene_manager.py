90b9879e590eb2a565e82de7829928601f3d7b7d
un5450 <un5450@outlook.com>
2023-08-17 12:30:37 +0900

    scenemanager

--- src/scene_manager.py
+++ src/scene_manager.py
@@ -1,1 +1,33 @@
-# WIP (추가예정)
+from scenes.scene import Scene
+from pygame import Surface
+
+class SceneManager():
+    _scene_stack: list[Scene] = []
+
+    @classmethod
+    def _current_scene(cls) -> (Scene | None):
+        stack = cls._scene_stack
+        if not stack:
+            return None
+        else:
+            return stack[len(stack) - 1]
+
+    @classmethod
+    def update(cls):
+        scene = cls._current_scene()
+        if scene:
+            scene.update()
+    
+    @classmethod
+    def draw(cls, surface: Surface):
+        scene = cls._current_scene()
+        if scene:
+            scene.draw(surface)
+
+    @classmethod
+    def push_scene(cls, scene: Scene):
+        cls._scene_stack.append(scene)
+    
+    @classmethod
+    def clear_scene(cls):
+        cls._scene_stack.clear()
---
+++ src/scenes/scene.py
@@ -0,0 +1,14 @@
+from pygame import Surface
+
+class Scene():
+    """Scene은 게임에 등장할 '장면'이다.
+    타이틀 화면, 일시정지 화면, 게임 화면 등을 Scene으로 구현할 수 있다.
+    """
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, surface: Surface):
+        pass
