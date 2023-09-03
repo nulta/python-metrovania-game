41a731aac7a6ad6a1ba6c55372808ee28fb95d82
un5450 <un5450@outlook.com>
2023-09-03 22:33:04 +0900

    물리학 싫어

--- src/entities/components/physics_component.py
+++ src/entities/components/physics_component.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Sequence
 import game_globals
 import debug
 from constants import TILE_SIZE
+from math import isnan, copysign
 
 if TYPE_CHECKING:
     from entity import Entity
@@ -46,217 +47,151 @@ class PhysicsComponent:
 
 
     def update(self):
-        # print("===== NEW FRAME =====")
         self._update_gravity()
-        self.owner.position += self.velocity * game_globals.delta_time
-        self._update_collision()
+        self._update_position()
+        self._check_stuck()
     
-    def _update_collision(self):
-        # 지금은 일단 맵과의 충돌만 처리하고 있다.
+    def _update_position(self):
+        if not self.velocity:
+            return
 
-        vel = self.velocity
-
-        collided = False
-        rep_x = 0.0
-        rep_y = 0.0
-        # 최대 10번 반복한다
-        # for _ in range(10):
-        #     collided = False
-        #     for rect in self._get_collide_rects():
-        #         repulse_vec = self._get_repulse_vector(vel, rect)
-        #         if repulse_vec:
-        #             print(repulse_vec)
-        #             self.owner.position += repulse_vec
-        #             vel = Vector2(0, 0)
-        #             collided = True
-
-        #     # 아무하고도 충돌하지 않았다면, 루프에서 빠져나간다
-        #     if not collided:
-        #         break
+        old_pos = self.owner.position
+        new_pos = old_pos + (self.velocity * game_globals.delta_time)
+        new_pos.x = int(new_pos.x)
+        new_pos.y = int(new_pos.y)
         
-        # if collided:
-        #     # 방금 루프는, 10회 제한이 없었다면 무한 루프로 빠졌을 수도 있다
-        #     # 별로 좋지 않은 경우이다. 경고를 해 주자.
-        #     print(f"PhysicsComponent of {repr(self.owner)}: Failed to solve the collisions"
-        #           + "in 10 times. Possible physics error?")
+        old_hitbox: "Rect | None" = self.owner.get("hitbox")
+        if not old_hitbox:
+            return
+        new_hitbox = old_hitbox.move(new_pos - old_pos)
 
-        orig_pos = self.owner.position
+        possible_colliding_rects = self._get_collide_rects()
+        lose_x_vel = False  # X방향 속도를 잃어야 하는가? (즉, X방향으로 벽에 박았는가?)
+        lose_y_vel = False  # Y방향 속도를 잃어야 하는가? (즉, Y방향으로 벽에 박았는가?)
+        loops = 0
+        while True:
+            collided = False
+
+            # Discrete 방식으로 충돌하는지 판정
+            real_colliding_rects: "list[Rect]" = []
+            for rect in possible_colliding_rects:
+                if not new_hitbox.colliderect(rect):
+                    continue
+                real_colliding_rects.append(rect)
+            
+            # 충돌하는 것으로 판정된 Rect들을 거리에 따라 정렬
+            real_colliding_rects.sort(key=lambda rect: Vector2(rect.center).distance_squared_to(new_hitbox.center))
+
+            for rect in real_colliding_rects:
+                # 현재 상태에서 충돌하는지 판별
+                # new_pos는 변경되었을 수도 있다.
+                delta_pos = new_pos - old_pos
+                new_hitbox = old_hitbox.move(delta_pos)
+                if not new_hitbox.colliderect(rect):
+                    continue
+                if not delta_pos:
+                    continue
+
+                # 충돌한다면, 충돌 해결
+                collided = True
+                infinite = float("inf")  # 무한대
+
+                # X 방향으로 옮겨서 충돌을 해결할 때, 옮겨야 할 픽셀 수
+                diff_x = infinite
+                if 0 < delta_pos.x:    # X-->
+                    diff_x = int(new_hitbox.right - rect.left)
+                elif delta_pos.x < 0:  # <--X
+                    diff_x = int(rect.right - new_hitbox.left)
+
+                assert diff_x >= 0
+                
+                # if diff_x < 0:
+                #     diff_x = 0
+
+                # Y 방향으로 옮겨서 충돌을 해결할 때, 옮겨야 할 픽셀 수
+                diff_y = infinite
+                if 0 < delta_pos.y:    # Y going down
+                    diff_y = int(new_hitbox.bottom - rect.top)
+                elif delta_pos.y < 0:  # Y going up
+                    diff_y = int(rect.bottom - new_hitbox.top)
+                
+                assert diff_y >= 0
+                # if diff_y < 0:
+                #     diff_y = infinite
+
+                assert min(diff_x, diff_y) != infinite
+
+                # 벡터의 내적
+                dot_x = abs(delta_pos.dot((diff_x, 0)))
+                dot_y = abs(delta_pos.dot((0, diff_y)))
+
+                if isnan(dot_x): dot_x = infinite
+                if isnan(dot_y): dot_y = infinite
+
+                assert not (dot_x == infinite and dot_y == infinite)
+
+                # 더 적게 밀어도 되는 쪽으로 민다.
+                if dot_x < dot_y:
+                    # X쪽으로 반작용을 가한다.
+                    assert delta_pos.x
+                    lose_x_vel = True
+                    diff_x = min(abs(diff_x), abs(delta_pos.x))  # 작용력보다 수직 항력이 크면 안 된다
+                    new_pos.x -= copysign(diff_x, delta_pos.x)
+                else:
+                    # Y쪽으로 반작용을 가한다.
+                    lose_y_vel = True
+                    diff_y = min(abs(diff_y), abs(delta_pos.y))  # 작용력보다 수직 항력이 더 크면 안 된다
+                    new_pos.y -= copysign(diff_y, delta_pos.y)
+            
+            if not collided:
+                break
+
+            loops += 1
+            if loops >= 5:
+                print("PhysicsComponent: Failed to resolve collisions in 5 times! possible phys error?")
+                break
+            
+        self.owner.position = new_pos
+        if lose_x_vel:
+            self.velocity.x = 0
+        if lose_y_vel:
+            self.velocity.y = 0
+    
+    def _check_stuck(self):
+        """속도가 0이고 벽에 끼었을 경우, 바깥으로 나간다."""
+        if self.velocity:
+            return
 
         for rect in self._get_collide_rects():
-            repulse_vec = self._get_repulse_vector(vel, rect)
-            if repulse_vec.x:
-                rep_x = repulse_vec.x
-            if repulse_vec.y:
-                rep_y = repulse_vec.y
-        
-        if rep_x and rep_y:
-
-            if rep_x > rep_y:
-                # X만 움직여도 되지 않을까?
-                has_collision = False
-                vel2 = Vector2(vel.x, 0)
-                self.owner.position = orig_pos + Vector2(0, rep_y)
-                for rect in self._get_collide_rects():
-                    # TODO: optimize. 여기선 충돌하는지 여부만 판정해도 되는데 불필요하게 연산을 하고 있다.
-                    repulse_vec = self._get_repulse_vector(vel2, rect)
-                    if repulse_vec:
-                        has_collision = True
-                        continue
-
-                # Y만 움직여도 되지 않을까?
-                if has_collision:
-                    has_collision = False
-                    vel2 = Vector2(0, vel.y)
-                    self.owner.position = orig_pos + Vector2(rep_x, 0)
-                    for rect in self._get_collide_rects():
-                        # TODO: optimize. 여기선 충돌하는지 여부만 판정해도 되는데 불필요하게 연산을 하고 있다.
-                        repulse_vec = self._get_repulse_vector(vel2, rect)
-                        if repulse_vec:
-                            has_collision = True
-                            continue
-                
-                if has_collision:
-                    # 앗, 무조건 둘 다 움직여야 하는구나.
-                    vel = Vector2(0, 0)
-                    self.owner.position.x = orig_pos.x + rep_x
-                    self.owner.position.y = orig_pos.y + rep_y
-                else:
-                    vel = vel2
-            else:
-                # Y만 움직여도 되지 않을까?
-                has_collision = False
-                vel2 = Vector2(0, vel.y)
-                self.owner.position = orig_pos + Vector2(rep_x, 0)
-                for rect in self._get_collide_rects():
-                    # TODO: optimize. 여기선 충돌하는지 여부만 판정해도 되는데 불필요하게 연산을 하고 있다.
-                    repulse_vec = self._get_repulse_vector(vel2, rect)
-                    if repulse_vec:
-                        has_collision = True
-                        continue
-
-                # X만 움직여도 되지 않을까?
-                if has_collision:
-                    has_collision = False
-                    vel2 = Vector2(vel.x, 0)
-                    self.owner.position = orig_pos + Vector2(0, rep_y)
-                    for rect in self._get_collide_rects():
-                        # TODO: optimize. 여기선 충돌하는지 여부만 판정해도 되는데 불필요하게 연산을 하고 있다.
-                        repulse_vec = self._get_repulse_vector(vel2, rect)
-                        if repulse_vec:
-                            has_collision = True
-                            continue
-                
-                if has_collision:
-                    # 앗, 무조건 둘 다 움직여야 하는구나.
-                    vel = Vector2(0, 0)
-                    self.owner.position.x = orig_pos.x + rep_x
-                    self.owner.position.y = orig_pos.y + rep_y
-                else:
-                    vel = vel2
-        else:
-            if rep_x:
-                vel = Vector2(0, vel.y)
-                self.owner.position.x += rep_x
-            elif rep_y:
-                vel = Vector2(vel.x, 0)
-                self.owner.position.y += rep_y
-                
-
-            
-        
-        # Velocity를 업데이트해준다.
-        self.velocity = vel
-        # self.velocity = Vector2(0,0)
-
-    def _get_repulse_vector(self, vel: "Vector2", target_rect: "Rect"):
-        # vel이 영벡터라면 영벡터를 반환한다
-        if not vel: return Vector2(0, 0)
-
-        # 현재 시점의 히트박스 Rect
-        rect_0: "Rect | None" = self.owner.get("hitbox")
-
-        # 히트박스가 없으면 충돌 판정이 없으므로 영벡터를 반환한다
-        if not rect_0: return Vector2(0, 0)
-
-        # 이동 이후의 히트박스 Rect
-        rect_1 = rect_0.move(vel * game_globals.delta_time)
-
-        # target_rect와 충돌하지 않는다면, 반발할 필요가 없으므로 영벡터를 반환한다
-        if not rect_1.colliderect(target_rect): return Vector2(0, 0)
-
-        # 정규화된(길이가 1인) vel 벡터
-        vel_norm = vel.normalize()
-
-        # "반발 벡터"
-        repulse_vec = Vector2(0, 0)
-
-        # rect의 변과 target의 변 사이 X좌표의 차
-        diff_x = None
-        # 반발 벡터의 길이. 기본값은 무한대.
-        repulse_x_len = float("inf")
-        if vel_norm.x < 0:
-            # vel 벡터가 왼쪽으로 가고 있다
-            # rect의 왼쪽 변과 target의 오른쪽 변에 대해서 X좌표의 차를 계산
-            diff_x = target_rect.right - rect_1.left
-        elif vel_norm.x > 0:
-            # vel 벡터가 오른쪽으로 가고 있다
-            # rect의 오른쪽 변과 target의 왼쪽 변에 대해서 X좌표의 차를 계산
-            diff_x = target_rect.left - rect_1.right
-        
-        if diff_x is not None:
-            # repulse_x 벡터와 그 길이를 계산해낸다
-            # repulse_x = vel_norm * (diff_x / vel_norm.x)
-            repulse_x = Vector2(diff_x, vel_norm.y * diff_x / vel_norm.x)
-            repulse_x_len = repulse_x.length()
-
-        # rect의 변과 target의 변 사이 Y좌표의 차
-        diff_y = None
-        # 반발 벡터의 길이. 기본값은 무한대.
-        repulse_y_len = float("inf")
-        if vel_norm.y > 0:
-            # vel 벡터가 아래쪽으로 가고 있다
-            # rect의 아래쪽 변과 target의 위쪽 변에 대해서 Y좌표의 차를 계산
-            diff_y = target_rect.top - rect_1.bottom
-        elif vel_norm.y < 0:
-            # vel 벡터가 위쪽으로 가고 있다
-            # rect의 위쪽 변과 target의 아래쪽 변에 대해서 Y좌표의 차를 계산
-            diff_y = target_rect.bottom - rect_1.top
-        
-        if diff_y is not None:
-            # repulse_y 벡터와 그 길이를 계산해낸다
-            # repulse_y = vel_norm * (diff_y / vel_norm.y)
-            repulse_y = Vector2(vel_norm.x * diff_y / vel_norm.y, diff_y)
-            repulse_y_len = repulse_y.length()
-        
-        # repulse_y와 repulse_x 중 더 크기가 작은 벡터를 찾는다
-        if repulse_y_len > repulse_x_len:
-            # X축 방향으로 밀어낸다
-            repulse_vec = Vector2(diff_x or 0, 0)
-        elif repulse_x_len > repulse_y_len:
-            # Y축 방향으로 밀어낸다
-            repulse_vec = Vector2(0, diff_y or 0)
-        
-        repulse_vec = Vector2(int(repulse_vec.x), int(repulse_vec.y))
-        
-        # debug.rect(rect_1, color=(0, 0, 255))
-        # debug.line(rect_1.center, rect_1.center + repulse_vec, color=(255, 0, 0))
-        
-        return repulse_vec
+            hitbox: "Rect | None" = self.owner.get("hitbox")
+            if not hitbox:
+                return
+            if rect.colliderect(hitbox):
+                # 끼었다!
+                self.owner.position += Vector2(0, -60)
 
     def _update_gravity(self):
         self.velocity += self._gravity * game_globals.delta_time
 
     def _get_collide_rects(self):
-        rects = []
-        # Static Tilemap에서 rect들을 구워낸다
-        # TODO: owner 근처에 있는 rect들만 뽑아내도록 하기
+        rects: "list[Rect]" = []
+
+        # Static Tilemap에서 근처에 있는 rect들을 구워낸다
         tile_size = self._tile_size
         for y, row in enumerate(self._static_tilemap):
             for x, tile_exists in enumerate(row):
-                if tile_exists:
+                if tile_exists and self._is_nearby_tile(x, y):
                     rects.append(Rect(x * tile_size, y * tile_size, tile_size, tile_size))
 
-        for rect in rects:
-            debug.rect(rect)
-
         return rects
+
+    def _is_nearby_tile(self, tile_x, tile_y):
+        my_pos = self.owner.position
+        tile_size = self._tile_size
+        distance_max_sqr = (tile_size * 3) ** 2
+        tile_pos = (tile_x * tile_size + (tile_size / 2), tile_y * tile_size + (tile_size / 2))
+        distance_sqr = my_pos.distance_squared_to(tile_pos)
+        if distance_sqr <= distance_max_sqr:
+            return True
+        else:
+            return False
