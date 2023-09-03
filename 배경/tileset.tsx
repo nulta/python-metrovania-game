7ffcdee5e30b821c801d37062d3e04338759b101
nicole <dldkdud0771@gmail.com>
2023-09-04 08:48:48 +0900

    배경 추가

---
+++ 배경/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset" tilewidth="30" tileheight="30" tilecount="75" columns="5">
+ <image source="../../../../Downloads/타일셋(완).png" width="150" height="450"/>
+</tileset>
