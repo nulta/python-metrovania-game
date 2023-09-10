8593bed7cb0d25d9021b347038c448affbd72c09
nicole <dldkdud0771@gmail.com>
2023-09-11 04:01:00 +0900

    크래딧 넣을 준비

--- src/scenes/credits_scene.py
+++ src/scenes/credits_scene.py
@@ -15,7 +15,6 @@ class CreditsScene(Scene):
     """CreditsScene은 게임 타이틀 화면이다."""
 
     _music = ""
-    Text= "ㅇ\nㄹ\n나\n얼\n아\n러\nㅏ\nㄴ\nㅁ\nㅇ\n리\nㅏ\nㄴ\n어\n라\nㅣ\n넝\n라\nㅓ\nㅁ\nㄴ\n러\n나\nㅣ\nㅇ\n러\nㅏ\nㅣ\nㄴ\n어\n리\nㅏ\nㅁ\nㅇ\n너\n라\nㅣ\n넝\nㅁ\n라\nㅣ\n넘\nㅇ\n라\nㅣ\n"
     def __init__(self):
         super().__init__()
 
@@ -25,28 +24,25 @@ class CreditsScene(Scene):
         # 메뉴창 UI와 관련된 변수들.
     def update(self):
         pass
-    def draw(self, surface: pygame.Surface):
-        self.draw_background(surface)
-        self.draw_text(surface)
-    def draw_background(self, surface: pygame.Surface):
-        # 배경 그리기
-        scence_path = ResourceLoader.load_image(f"background/black.png")
-        surface.blit(scence_path, (0, 0))
+    def draw_parallax(self, surface: "Surface", image_name: "str", parallax_mul: "float",
+                      t: "float", to_end=False, y_offset=0.0):
+        image = ResourceLoader.load_image(image_name)
 
-    def draw_text(self, surface: pygame.Surface):
+        initial_offset = -Vector2(0, 1490 - 600)
+        offset = initial_offset - Vector2(0, t * initial_offset.y * parallax_mul)
+        offset += Vector2(0, y_offset)
+        if to_end:
+            offset += Vector2(0, initial_offset.y * parallax_mul - initial_offset.y)
+
+        surface.blit(image, offset)
+                     
+    def draw(self, surface: pygame.Surface):
         t = util.on_keyframes(self.scene_time, {
             0.0: 1,
             8.0: 0,
-        }, easein=True, easeout=True)
+        }, easein=False, easeout=False)
 
-
-        Fonts.get("bold").render_to(
-            surface,
-            (300, 555),
-            self.Text,
-            fgcolor=(255, 255, 255),
-            size=15,
-        )
+        self.draw_parallax(surface, "background/intro/sky.png", 1.0, t)
 
     def on_destroy(self):
         # 음악은 다음 씬과 이어진다.
